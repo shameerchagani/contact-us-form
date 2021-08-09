a5fd0dcdd3271331e44d9fd8b0df833018e43795
shameer <shameerchagani@gmail.com>
2021-08-09 12:58:34 +0300

    Contact Us form, With Send email using Gmail Account.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Import Bootstrap and app.css
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+//Components Import
+import ContactForm from './compnents/ContactForm';
+import {Header} from './compnents/Header'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <div className="col-md-6 offset-md-3">
+          <ContactForm />
+        </div>
+      </div>
+    </>
   );
 }
 
